7be909df1557ce0bc4ee589ce548dd4dd184ae12
DarshanKrishna-DK <darshankrishna2k2@gmail.com>
2023-07-31 08:23:35 +0530

    Scroll Reveal and Validation Implemented

--- script.js
+++ script.js
@@ -54,14 +54,61 @@ setInterval(nextSlide, 8000);
 
 /*----------------Scroll Reveal----------------*/
 
-// ScrollReveal({
-//     reset: true,
-//     distance: '80px',
-//     duration: 2000,
-//     delay: 200
-// })
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+})
 
-// ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
-// ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom'});
-// ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left'});
-// ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right'});
+ScrollReveal().reveal('h2', { origin: 'top'});
+ScrollReveal().reveal('.projects-container, .container', { origin: 'bottom'});
+ScrollReveal().reveal('.contribute-content, .contact form ', { origin: 'left'});
+ScrollReveal().reveal('.about-content, #contribute_form', { origin: 'right'});
+
+
+/*----------------Validation---------------- */
+
+// Email validation function
+function validateEmail(email) {
+    var emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailPattern.test(email);
+  }
+  
+// Mobile number validation function
+function validatePhoneNumber(mobileNumber) {
+    var mobilePattern = /^[6-9]\d{9}$/; 
+    return mobilePattern.test(mobileNumber);
+}
+
+// Contact Us Input Validation
+function validateForm() {
+    var emailInput = document.getElementById("email");
+    var mobileNumberInput = document.getElementById("mobileNumber");
+  
+    if (!validatePhoneNumber(mobileNumberInput.value)) {
+      alert("Invalid mobile number!");
+      return false;
+    }
+
+    if (!validateEmail(emailInput.value)) {
+        alert("Invalid email address!");
+        return false;
+    }
+  
+    // If both email and mobile number inputs are valid, allow form submission
+    return true;
+}
+
+// Contribute Input Validation
+function validateContributeForm() {
+    var mobileNumberInput = document.getElementById("mobileNum");
+  
+    if (!validatePhoneNumber(mobileNumberInput.value)) {
+      alert("Invalid mobile number!");
+      return false;
+    }
+  
+    // If mobile number is valid, allow form submission
+    return true;
+}
